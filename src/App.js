4db7e4bac109d626f24c9e01e545fe28f2d2b4ef
zahra-p <zahra.adelinia@gmail.com>
2021-12-29 13:22:28 +0330

    removed unnecessary files and added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="/" target="_blank" >Zahra Adelinia</a>
+        {" "}
+        and is {" "}
+        <a href="https://github.com/zahra-p/react-weather"> Open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
