caa9d2f7b6e406edee956a488efd8a79b16cc0b3
zahra-p <zahra.adelinia@gmail.com>
2021-12-29 18:06:40 +0330

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
+       
       <footer>
         This project was coded by{" "}
         <a href="/" target="_blank" >Zahra Adelinia</a>
@@ -12,6 +14,7 @@ function App() {
         and is {" "}
         <a href="https://github.com/zahra-p/react-weather"> Open-sourced on GitHub</a>
       </footer>
+      </div>  
     </div>
   );
 }
